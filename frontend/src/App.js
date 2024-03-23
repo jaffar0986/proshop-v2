ce9d4eae750dc23a91b59a6b85ec379156d280e9
Jaffar <jaffar@example.com>
2024-03-23 13:20:35 +0530

    product lisitng and routes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div>
-      Welcome to proshop
-    </div>
+    <>
+        <Header />
+        <main className='py-3'>
+          <Container>
+          <Outlet />
+          </Container>
+        </main>
+        <Footer />
+    </>
   )
 }
 
