88f050d5befca4123725b753bde40f9eb6544d8a
Jaffar <jaffar@example.com>
2024-03-24 08:50:56 +0530

    data seeder done

--- backend/data/user.js
+++ backend/data/user.js
@@ -3,19 +3,19 @@ import bcrypt from "bcryptjs"
 const users = [
     {
         name: 'Admin User',
-        eamil: 'admin@email.com',
+        email: 'admin@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: true
     },
     {
         name: 'Mohammad Ali',
-        eamil: 'ali@email.com',
+        email: 'ali@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: false
     },
     {
         name: 'Irtika Ahmad',
-        eamil: 'ahmad@email.com',
+        email: 'ahmad@email.com',
         password: bcrypt.hashSync('123456', 10),
         isAdmin: false
     }
