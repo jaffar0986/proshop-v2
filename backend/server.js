477ddc5947063622b1396145d63572c89ec67865
Jaffar <jaffar@example.com>
2024-03-26 10:08:18 +0530

    User validation done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorsHandler } from "./middleware/errorHandler.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js"
 
 const port = process.env.PORT || 5000;
 
@@ -12,11 +13,16 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send("API is running");
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorsHandler);
